3ec43baf5fcffc7e17474a86345e6415a2ed3b00
YogDPatil <ydpatil.it@gmail.com>
2025-06-08 22:26:44 +0530

    set browser as parameters

--- src/test/java/com/ui/tests/TestBase.java
+++ src/test/java/com/ui/tests/TestBase.java
@@ -2,19 +2,32 @@ package com.ui.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import com.beust.jcommander.Parameter;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public abstract class TestBase {
 	private WebDriver driver;
 
+	@Parameters("browser")
 	@BeforeMethod(alwaysRun = true)
-	public void driverSetup() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+	public void driverSetup(@Optional("chrome") String browser) {
+		if (browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else {
+			System.out.println(browser + " is not compatible");
+		}
 		driver.manage().window().maximize();
 		driver.get("http://phoenix.techwithjatin.com/sign-in");
 	}
