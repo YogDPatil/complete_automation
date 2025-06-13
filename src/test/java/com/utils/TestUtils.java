dffdc7a7a3433945ee4febae1fcd6548c007d21a
YogDPatil <ydpatil.it@gmail.com>
2025-06-13 15:21:14 +0530

    config folder added

---
+++ src/test/java/com/utils/TestUtils.java
@@ -0,0 +1,19 @@
+package com.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class TestUtils {
+
+//	public static void getValueFromPropertiesFile() {
+//		FileInputStream fis;
+//		try {
+//			fis = new FileInputStream(
+//					new File(System.getProperty("user.dir") + "/src/test/resources/config/config.properties"));
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//	}
+
+}
