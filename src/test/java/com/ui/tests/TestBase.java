1608c0fad3a356b67c3f33c7758f80a5013761b9
YogDPatil <ydpatil.it@gmail.com>
2025-06-16 16:46:21 +0530

    dev env properties file added

--- src/test/java/com/ui/tests/TestBase.java
+++ src/test/java/com/ui/tests/TestBase.java
@@ -1,9 +1,10 @@
 package com.ui.tests;
 
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterMethod; 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
