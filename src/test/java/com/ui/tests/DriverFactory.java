bdbdf5983a706b640e1f4f7e109190acc1549f6f
YogDPatil <ydpatil.it@gmail.com>
2025-08-08 11:28:44 +0530

    created pure singleton class driver factory

--- src/test/java/com/ui/tests/DriverFactory.java
+++ src/test/java/com/ui/tests/DriverFactory.java
@@ -15,72 +15,78 @@ import org.openqa.selenium.safari.SafariDriver;
 
 import java.lang.reflect.Method;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DriverFactory {
 
+    private static DriverFactory instance;
     private static ThreadLocal<WebDriver> thrdlclDriver = new ThreadLocal<>();
 
     private DriverFactory() {
+
+    }
+
+    public static void getInstance() {
+        if (instance == null) {
+            instance = new DriverFactory();
+        }
+    }
+
+    public static void setDriver(String browser, boolean headless, boolean isRemote, String browserVersion, Env env, Method method) throws Exception {
+        if (getDriver() == null) {
+            if (isRemote) {
+                MutableCapabilities caps = new MutableCapabilities();
+                Map<String, Object> bstackOptions = new HashMap<>();
+                caps.setCapability("browserName", browser);
+                caps.setCapability("browserVersion", browserVersion);
+                bstackOptions.put("os", "Windows");
+                bstackOptions.put("osVersion", "10");
+                bstackOptions.put("userName", TestUtils.getValueFromPropertiesFile(env, ConfigConst.BS_USERNAME));
+                bstackOptions.put("accessKey", TestUtils.getValueFromPropertiesFile(env, ConfigConst.BS_ACCESS_KEY));
+                bstackOptions.put("sessionName", method.getName());
+                caps.setCapability("bstack:options", bstackOptions);
+                thrdlclDriver.set(new RemoteWebDriver(new URI(TestUtils.getValueFromPropertiesFile(env, ConfigConst.BS_URL)).toURL(), caps));
+            } else {
+                if (getDriver() == null) {
+                    switch (browser.toLowerCase()) {
+                        case "chrome":
+                            WebDriverManager.chromedriver().setup();
+                            ChromeOptions cOpt = new ChromeOptions();
+                            if (headless) {
+                                cOpt.addArguments("--headless");
+                            }
+                            thrdlclDriver.set(new ChromeDriver(cOpt));
+                            break;
+                        case "firefox":
+                            WebDriverManager.firefoxdriver().setup();
+                            FirefoxOptions ffOpt = new FirefoxOptions();
+                            if (headless) {
+                                ffOpt.addArguments("--headless");
+                            }
+                            thrdlclDriver.set(new FirefoxDriver(ffOpt));
+                            break;
+                        case "safari":
+                            WebDriverManager.safaridriver().setup();
+                            thrdlclDriver.set(new SafariDriver());
+                            break;
+                        default:
+                            throw new Exception("Error: " + browser + " is not compatible");
+                    }
+                }
+            }
+        }
     }
 
     public static WebDriver getDriver() {
         return thrdlclDriver.get();
     }
 
-    public static void setDriver(String browser, boolean headless, boolean isRemote, String browserVersion, Env env, Method method) throws Exception {
-        if (isRemote) {
-            MutableCapabilities caps = new MutableCapabilities();
-            Map<String, Object> bstackOptions = new HashMap<>();
-            caps.setCapability("browserName", browser);
-            caps.setCapability("browserVersion", browserVersion);
-            bstackOptions.put("os", "Windows");
-            bstackOptions.put("osVersion", "10");
-            bstackOptions.put("userName", TestUtils.getValueFromPropertiesFile(env, ConfigConst.BS_USERNAME));
-            bstackOptions.put("accessKey", TestUtils.getValueFromPropertiesFile(env, ConfigConst.BS_ACCESS_KEY));
-            bstackOptions.put("sessionName", method.getName());
-            caps.setCapability("bstack:options", bstackOptions);
-            thrdlclDriver.set(new RemoteWebDriver(new URI(TestUtils.getValueFromPropertiesFile(env, ConfigConst.BS_URL)).toURL(), caps));
-
-        } else {
-            if (getDriver() == null) {
-                switch (browser.toLowerCase()) {
-                    case "chrome":
-                        WebDriverManager.chromedriver().setup();
-                        ChromeOptions cOpt = new ChromeOptions();
-                        if (headless) {
-                            cOpt.addArguments("--headless");
-                        }
-                        thrdlclDriver.set(new ChromeDriver(cOpt));
-                        break;
-                    case "firefox":
-                        WebDriverManager.firefoxdriver().setup();
-                        FirefoxOptions ffOpt = new FirefoxOptions();
-                        if (headless) {
-                            ffOpt.addArguments("--headless");
-                        }
-                        thrdlclDriver.set(new FirefoxDriver(ffOpt));
-                        break;
-                    case "safari":
-                        WebDriverManager.safaridriver().setup();
-                        thrdlclDriver.set(new SafariDriver());
-                        break;
-                    default:
-                        throw new Exception("Error: " + browser + " is not compatible");
-                }
-            }
-        }
-    }
-
-    public static void setRemoteWebDriver(RemoteWebDriver remoteWebDriver) {
-        thrdlclDriver.set(remoteWebDriver);
-    }
-
     public static void quitDriver() {
-        if (thrdlclDriver != null) {
+        if (getDriver() != null) {
             getDriver().quit();
-            thrdlclDriver.remove();  // important to avoid memory leaks
         }
     }
+
 }
